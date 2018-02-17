9b1a82661d4ce8572f7f85d1c1de4e249ca4050a
harrisonmg <gieraltowski.har@gmail.com>
2018-02-17 13:28:45 -0500

    all functions now contained in header file. dynamic buffer clearing added.

--- examples/peripherals/timer_group/main/nubaja_esp32_helper.h
+++ examples/peripherals/timer_group/main/nubaja_esp32_helper.h
@@ -42,50 +42,78 @@
 */ 
 
 //I2C CONFIG
-#define I2C_EXAMPLE_MASTER_SCL_IO          22               /*!< gpio number for I2C master clock */
-#define I2C_EXAMPLE_MASTER_SDA_IO          23               /*!< gpio number for I2C master data  */
-#define I2C_NUM                            I2C_NUM_0        /*!< I2C port number for master dev */
-#define I2C_EXAMPLE_MASTER_TX_BUF_DISABLE  0                /*!< I2C master do not need buffer */
-#define I2C_EXAMPLE_MASTER_RX_BUF_DISABLE  0                /*!< I2C master do not need buffer */
-#define I2C_EXAMPLE_MASTER_FREQ_HZ         400000           /*!< I2C master clock frequency */
-#define WRITE_BIT                          I2C_MASTER_WRITE /*!< I2C master write */
-#define READ_BIT                           I2C_MASTER_READ  /*!< I2C master read */
-#define ACK_CHECK_EN                       0x1              /*!< I2C master will check ack from slave*/
-#define ACK_CHECK_DIS                      0x0              /*!< I2C master will not check ack from slave */
-#define ACK                                0x0              /*!< I2C ack value */
-#define NACK                               0x1              /*!< I2C nack value */
-#define DATA_LENGTH                        1                //in bytes
-#define I2C_TASK_LENGTH                    10              //in ms
+#define I2C_MASTER_SCL_IO                   22               /*!< gpio number for I2C master clock */
+#define I2C_MASTER_SDA_IO                   23               /*!< gpio number for I2C master data  */
+#define I2C_NUM                             I2C_NUM_0        /*!< I2C port number for master dev */
+#define I2C_MASTER_TX_BUF_DISABLE           0                /*!< I2C master do not need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE           0                /*!< I2C master do not need buffer */
+#define I2C_CLK_HZ                          400000           /*!< I2C master clock frequency */
+#define WRITE_BIT                           I2C_MASTER_WRITE /*!< I2C master write */
+#define READ_BIT                            I2C_MASTER_READ  /*!< I2C master read */
+#define ACK_CHECK_EN                        0x1              /*!< I2C master will check ack from slave*/
+#define ACK_CHECK_DIS                       0x0              /*!< I2C master will not check ack from slave */
+#define ACK                                 0x0              /*!< I2C ack value */
+#define NACK                                0x1              /*!< I2C nack value */
+#define DATA_LENGTH                         1                //in bytes
+#define I2C_TASK_LENGTH                     10              //in ms
 
 //errors
-#define SUCCESS         0
-#define I2C_READ_FAILED 1
-#define FILE_DUMP_ERROR 2
+#define SUCCESS                             0
+#define I2C_READ_FAILED                     1
+#define FILE_DUMP_ERROR                     2
 
 //sad card spi config
-#define PIN_NUM_MISO 18
-#define PIN_NUM_MOSI 19
-#define PIN_NUM_CLK  14
-#define PIN_NUM_CS   15
+#define PIN_NUM_MISO                        18
+#define PIN_NUM_MOSI                        19
+#define PIN_NUM_CLK                         14
+#define PIN_NUM_CS                          15
 
 //ITG-3200 register mappings for gyroscope
-#define XH                                 0x1D
-#define XL                                 0x1E
-#define YH                                 0x1F
-#define YL                                 0x20
-#define ZH                                 0x21
-#define ZL                                 0x22
+#define XH                                  0x1D
+#define XL                                  0x1E
+#define YH                                  0x1F
+#define YL                                  0x20
+#define ZH                                  0x21
+#define ZL                                  0x22
 
 //buffer config
-#define SIZE    100
-#define HEX     16
+#define SIZE                                50
+
+//TIMER CONFIGS
+#define TIMER_DIVIDER               16  //  Hardware timer clock divider
+#define TIMER_SCALE                 (TIMER_BASE_CLK / TIMER_DIVIDER)  // convert counter value to seconds
+#define CONTROL_LOOP_FREQUENCY      1   // control loop period for timer group 0 timer 0 in seconds
+#define PROGRAM_LENGTH              10 // program length for timer group 0 timer 1 in seconds
+
+//ADC CONFIGS
+#define V_REF               1000
+#define V_FS                3.6 //change accordingly to ADC_ATTEN_xx_x
+#define ADC_SCALE           (V_FS/4096)
+#define ATTENUATION         ADC_ATTEN_11db
+
+
+/*****************************************************/
+
 
 /* 
 * globals
 */ 
+
 extern const char *TAG;
-extern char f_buf[SIZE];
-extern char err_buf[SIZE];
+extern char f_buf[];
+extern char err_buf[];
+extern int buffer_idx;
+extern xQueueHandle ctrl_queue;
+extern SemaphoreHandle_t killSemaphore;
+
+//interrupt flag container
+typedef struct {
+    int ctrl_intr;
+} timer_event_t;
+
+/*****************************************************/
+
+
 
 /* HELPER FUNCTIONS
 * these functions do not interface with sensors
@@ -94,26 +122,10 @@ extern char err_buf[SIZE];
 *
 */
 
-/*
- * appends integer to the end of the buffer
- */
-void add_int_to_buffer (char buf[],int i_to_add) {
-    char formatted_string [sizeof(int)*8+1];
-    sprintf(formatted_string,"%04x",i_to_add);
-    strcat(buf,formatted_string);
-    printf("running buffer: %s\n",buf);
-}
-
 void record_error(char err_buffer[], char err_msg[]) {
     strcpy(err_buffer,err_msg);
 }
 
-/* CONTROL FLOW FUNCTIONS
-* these functions still do not interface with sensors but 
-* provide more functionality than manipulating data types, etc.
-*  
-*
-*/
 void ERROR_HANDLE_ME(int err_num) {
     char msg[50];
     switch (err_num) {
@@ -135,24 +147,72 @@ void ERROR_HANDLE_ME(int err_num) {
 /*
  * writes data buffer to a file on SD card
  */
-int dump_to_file(char buffer[],char err_buffer[]) {
-    // char nullstr[] = "\0";
-    // strcpy(buffer,nullstr);
+int dump_to_file(char buffer[],char err_buffer[],bool unmount) {
     FILE *fp;
-    fp = fopen("/sdcard/test.txt", "w");
+    fp = fopen("/sdcard/test.txt", "a");
     if (fp == NULL)
     {
         ESP_LOGE(TAG, "Failed to open file for writing");
         return FILE_DUMP_ERROR;
     }   
     fputs(buffer, fp);
+    printf("final buffer: %s\n",buffer);
     // fputs(err_buffer, fp);
+    // printf("err buffer: %s\n",err_buffer);
         
-    ESP_LOGI(TAG, "dumped");
     fclose(fp);
-    esp_vfs_fat_sdmmc_unmount();
+    if (unmount) {
+        esp_vfs_fat_sdmmc_unmount();
+        ESP_LOGI(TAG, "umounted & buffer dumped");
+        return SUCCESS;
+    }
+
+    ESP_LOGI(TAG, "buffer dumped");
     return SUCCESS;
 }
+
+
+/*
+ * appends 16b integer to the end of the buffer
+ * adds 2 hex digits to the end of the buffer
+ * designed for use with I2C reads of the itg-3200
+ * which has 16b registers
+ */
+void add_16b_to_buffer (char buf[],uint16_t i_to_add) {
+    char formatted_string [17]; //number of bits + 1
+    sprintf(formatted_string,"%04x",i_to_add);
+    strcat(buf,formatted_string);
+    buffer_idx+=4;
+    if (buffer_idx >= SIZE) {
+       buffer_idx = 0;
+       ERROR_HANDLE_ME(dump_to_file(buf,err_buf,0)); 
+       memset(buf,0,strlen(buf));
+    }    
+}
+
+/*
+ * appends 32b integer to the end of the buffer
+ * adds 3 hex digits to the end of the buffer
+ * designed with use case of adc read in mind (12b resolution)
+ */
+void add_12b_to_buffer (char buf[],uint16_t i_to_add) {
+    char formatted_string [13]; //number of bits + 1
+    sprintf(formatted_string,"%03x",i_to_add);
+    strcat(buf,formatted_string);
+    buffer_idx+=3;
+    if (buffer_idx >= SIZE) {
+        buffer_idx = 0;
+        ERROR_HANDLE_ME(dump_to_file(buf,err_buf,0)); 
+        memset(buf,0,strlen(buf)); 
+    }   
+}
+
+
+
+/*****************************************************/
+
+
+
 
 /* SENSOR INTERFACE FUNCTIONS
 * these functions interface with sensors  
@@ -175,6 +235,7 @@ int itg_3200_write(uint8_t slave_address, uint8_t reg, uint8_t data) {
     ret = i2c_master_cmd_begin(I2C_NUM, cmd, I2C_TASK_LENGTH / portTICK_RATE_MS); 
     i2c_cmd_link_delete(cmd);  
     if (ret != ESP_OK) {
+        ESP_LOGE(TAG,"i2c write failed");
         return I2C_READ_FAILED; //dead sensor
     } else { 
         return SUCCESS;
@@ -207,40 +268,55 @@ int itg_read(int reg)
     i2c_master_stop(cmd);
     ret = i2c_master_cmd_begin(I2C_NUM, cmd, I2C_TASK_LENGTH / portTICK_RATE_MS); 
     i2c_cmd_link_delete(cmd);  
-    // int data = (*data_h << 8 | *data_l); //comment out for one byte read
-    int data = *data_l;
-    add_int_to_buffer(f_buf,data);
+    // uint16_t data = (*data_h << 8 | *data_l); //comment out for one byte read
+    uint16_t data = *data_l;
+    add_16b_to_buffer(f_buf,data);
     if (ret != ESP_OK) {
-        printf("i2c read failed\n");
+        ESP_LOGE(TAG,"i2c read failed");
         return I2C_READ_FAILED; //dead sensor
     } else {
-        // printf("data: %04x\n",*data_l); 
         return SUCCESS;
     }
 }
 
 /*
- * Turns off all GPIO pins
- */
-void gpio_kill(int pin)
+* function designed with variable number of arguments
+* Turns off all GPIO pins passed in as arguments
+*/
+void gpio_kill(int num,...)
 {
-    // ESP_LOGI(TAG, "gpio_kill");
-    gpio_set_level(pin, 0);
-    gpio_set_direction(pin, GPIO_MODE_INPUT);  
+    va_list valist;
+    va_start(valist, num);
+
+    for (int i=0;i<num;i++) {
+        gpio_set_level(va_arg(valist, int), 0);
+        gpio_set_direction(va_arg(valist, int), GPIO_MODE_INPUT);          
+    }
+    va_end(valist);   
 }
+
+/*
+* function designed with variable number of arguments
+* allows for adc reads of multiple channels without
+* several repetitive function calls
+* function reads a single channel from the adc
+* the raw value is from 0-4095 (12b resolution)
+* the raw value is then added to the buffer appropriately
+*/
 
 void read_adc(int num,...) 
 {  
     va_list valist;
+    uint16_t val_0;
 
     /* initialize valist for num number of arguments */
     va_start(valist, num);
 
     /* access all the arguments assigned to valist */
     for (int i = 0; i < num; i++) {
-        int val_0 = adc1_get_raw(va_arg(valist, int));
-        printf("val: %x\n",val_0);
-        add_int_to_buffer(f_buf,val_0);        
+        val_0 = adc1_get_raw(va_arg(valist, int));
+        // printf("val %d: %x\n",i,val_0);
+        add_12b_to_buffer(f_buf,val_0);        
     }
 
     /* clean memory reserved for valist */
@@ -248,7 +324,10 @@ void read_adc(int num,...)
 }
 
 
-/* SENSOR OR MODULE CONFIGURATION FUNCTIONS
+/*****************************************************/
+
+
+/* CONFIGURATION (SENSOR OR MODULE) FUNCTIONS
 * these functions take care of the configuration of either
 * ESP32 hardware modules or sensors the ESP32 is interfacing with
 * for example the I2C modules and subsequent I2C slave devices 
@@ -292,15 +371,15 @@ void itg_3200_config() {
     int i2c_master_port = I2C_NUM;
     i2c_config_t conf;
     conf.mode = I2C_MODE_MASTER;
-    conf.sda_io_num = I2C_EXAMPLE_MASTER_SDA_IO;
+    conf.sda_io_num = I2C_MASTER_SDA_IO;
     conf.sda_pullup_en = GPIO_PULLUP_DISABLE;//
-    conf.scl_io_num = I2C_EXAMPLE_MASTER_SCL_IO;
+    conf.scl_io_num = I2C_MASTER_SCL_IO;
     conf.scl_pullup_en = GPIO_PULLUP_DISABLE;//
-    conf.master.clk_speed = I2C_EXAMPLE_MASTER_FREQ_HZ;
+    conf.master.clk_speed = I2C_CLK_HZ;
     i2c_param_config(i2c_master_port, &conf);
     i2c_driver_install(i2c_master_port, conf.mode,
-                       I2C_EXAMPLE_MASTER_RX_BUF_DISABLE,
-                       I2C_EXAMPLE_MASTER_TX_BUF_DISABLE, 0);
+                       I2C_MASTER_RX_BUF_DISABLE,
+                       I2C_MASTER_TX_BUF_DISABLE, 0);
 }
 
 /*
@@ -358,5 +437,61 @@ void sd_config()
     }
     // esp_vfs_fat_sdmmc_unmount();
 }
+
+/*
+ * Timer group0 ISR handler
+ * sets ctrl_intr flag high each time alarm occurs, re-enables alarm and sends data to main program task
+ * also unblocks end_program task at appropriate time decided by PROGRAM_LENGTH
+ */
+void IRAM_ATTR timer_group0_isr(void *para) {
+    uint32_t intr_status = TIMERG0.int_st_timers.val;
+    timer_event_t evt;
+    if ((intr_status & BIT(0))) { //if alarm is true, set interrupt flag 
+        TIMERG0.int_clr_timers.t0 = 1; //clear timer interrupt bit
+        evt.ctrl_intr = 1; //set flag
+    } 
+    if ((intr_status & BIT(1))) { //if alarm is true, set interrupt flag 
+        TIMERG0.int_clr_timers.t1 = 1; //clear timer interrupt bit
+        xSemaphoreGiveFromISR(killSemaphore,NULL); //GIVE SEMAPHORE
+    }
+    TIMERG0.hw_timer[0].config.alarm_en = TIMER_ALARM_EN; //re-enable timer for timer 0 which is timing control loop
+
+    // send the event data back to the main program task
+    xQueueSendFromISR(ctrl_queue, &evt, NULL);
+}
+
+
+/*
+ * sets up timer group 0 timers 0 and 1
+ * timer 0 times the control loop, set up for auto reload upon alarm
+ * timer 1 times the entire program, does not reload on alarm
+ */
+void timer_setup(int timer_idx,bool auto_reload, double timer_interval_sec)
+{
+    /* Select and initialize basic parameters of the timer */
+    timer_config_t config;
+    config.divider = TIMER_DIVIDER;
+    config.counter_dir = TIMER_COUNT_UP;
+    config.counter_en = TIMER_PAUSE;
+    config.alarm_en = TIMER_ALARM_EN;
+    config.intr_type = TIMER_INTR_LEVEL;
+    config.auto_reload = auto_reload;
+    timer_init(TIMER_GROUP_0, timer_idx, &config);
+
+    /* Timer's counter will initially start from value below.
+       Also, if auto_reload is set, this value will be automatically reload on alarm */
+    timer_set_counter_value(TIMER_GROUP_0, timer_idx, 0x0);
+
+    /* Configure the alarm value and the interrupt on alarm. */
+    timer_set_alarm_value(TIMER_GROUP_0, timer_idx, timer_interval_sec * TIMER_SCALE);
+    timer_enable_intr(TIMER_GROUP_0, timer_idx);
+    timer_isr_register(TIMER_GROUP_0, timer_idx, timer_group0_isr, 
+        (void *) timer_idx, ESP_INTR_FLAG_IRAM, NULL);
+
+    timer_start(TIMER_GROUP_0, timer_idx);
+}
+
+
+/*****************************************************/
 
 #endif
--- examples/peripherals/timer_group/main/timer_group_example_main.c
+++ examples/peripherals/timer_group/main/timer_group_example_main.c
@@ -1,17 +1,5 @@
 #include "nubaja_esp32_helper.h" //all other includes located here
 
-//TIMER CONFIGS
-#define TIMER_DIVIDER               16  //  Hardware timer clock divider
-#define TIMER_SCALE                 (TIMER_BASE_CLK / TIMER_DIVIDER)  // convert counter value to seconds
-#define CONTROL_LOOP_FREQUENCY      1   // control loop period for timer group 0 timer 0 in seconds
-#define PROGRAM_LENGTH              10 // program length for timer group 0 timer 1 in seconds
-
-//ADC CONFIGS
-#define V_REF               1000
-#define V_FS                3.6 //change accordingly to ADC_ATTEN_xx_x
-#define ADC_SCALE           (V_FS/4096)
-#define ATTENUATION         ADC_ATTEN_11db
-
 //global vars
 int level = 0;
 SemaphoreHandle_t killSemaphore = NULL;
@@ -19,63 +7,7 @@ xQueueHandle ctrl_queue;
 const char *TAG = "ESP3";
 char f_buf[SIZE];
 char err_buf[SIZE];
-
-//interrupt flag container
-typedef struct {
-    int ctrl_intr;
-} timer_event_t;
-
-/*
- * Timer group0 ISR handler
- * sets ctrl_intr flag high each time alarm occurs, re-enables alarm and sends data to main program task
- * also unblocks end_program task at appropriate time decided by PROGRAM_LENGTH
- */
-void IRAM_ATTR timer_group0_isr(void *para) {
-    uint32_t intr_status = TIMERG0.int_st_timers.val;
-    timer_event_t evt;
-    if ((intr_status & BIT(0))) { //if alarm is true, set interrupt flag 
-        TIMERG0.int_clr_timers.t0 = 1; //clear timer interrupt bit
-        evt.ctrl_intr = 1; //set flag
-    } 
-    if ((intr_status & BIT(1))) { //if alarm is true, set interrupt flag 
-        TIMERG0.int_clr_timers.t1 = 1; //clear timer interrupt bit
-        xSemaphoreGiveFromISR(killSemaphore,NULL); //GIVE SEMAPHORE
-    }
-    TIMERG0.hw_timer[0].config.alarm_en = TIMER_ALARM_EN; //re-enable timer for timer 0 which is timing control loop
-
-    // send the event data back to the main program task
-    xQueueSendFromISR(ctrl_queue, &evt, NULL);
-}
-
-/*
- * sets up timer group 0 timers 0 and 1
- * timer 0 times the control loop, set up for auto reload upon alarm
- * timer 1 times the entire program, does not reload on alarm
- */
-void timer_setup(int timer_idx,bool auto_reload, double timer_interval_sec)
-{
-    /* Select and initialize basic parameters of the timer */
-    timer_config_t config;
-    config.divider = TIMER_DIVIDER;
-    config.counter_dir = TIMER_COUNT_UP;
-    config.counter_en = TIMER_PAUSE;
-    config.alarm_en = TIMER_ALARM_EN;
-    config.intr_type = TIMER_INTR_LEVEL;
-    config.auto_reload = auto_reload;
-    timer_init(TIMER_GROUP_0, timer_idx, &config);
-
-    /* Timer's counter will initially start from value below.
-       Also, if auto_reload is set, this value will be automatically reload on alarm */
-    timer_set_counter_value(TIMER_GROUP_0, timer_idx, 0x0);
-
-    /* Configure the alarm value and the interrupt on alarm. */
-    timer_set_alarm_value(TIMER_GROUP_0, timer_idx, timer_interval_sec * TIMER_SCALE);
-    timer_enable_intr(TIMER_GROUP_0, timer_idx);
-    timer_isr_register(TIMER_GROUP_0, timer_idx, timer_group0_isr, 
-        (void *) timer_idx, ESP_INTR_FLAG_IRAM, NULL);
-
-    timer_start(TIMER_GROUP_0, timer_idx);
-}
+int buffer_idx = 0;
 
 /*
  * This function is executed each time timer 0 ISR sets ctrl_intr high upon timer alarm
@@ -84,7 +16,7 @@ void timer_setup(int timer_idx,bool auto_reload, double timer_interval_sec)
 void control() {
     ESP_LOGI(TAG, "ctrl");
     // gpio_set_level(GPIO_NUM_4, 1);
-    read_adc(1,ADC1_CHANNEL_6);
+    read_adc(2,ADC1_CHANNEL_6,ADC1_CHANNEL_3); //first argument is number of arguments
     ERROR_HANDLE_ME(itg_read(0x0));
     // gpio_set_level(GPIO_NUM_4, 0);
 }
@@ -120,7 +52,7 @@ void end_program(void* task) {
                 vTaskDelay(10);
             }
             vTaskDelay(500); 
-            ERROR_HANDLE_ME(dump_to_file(f_buf,err_buf)); 
+            ERROR_HANDLE_ME(dump_to_file(f_buf,err_buf,1)); 
             ESP_LOGI(TAG, "suspending task");
             vTaskSuspend(NULL);
         }
