d97fdd1bc8d71db0d87c1990d7bdc0ef2571afbc
harrisonmg <gieraltowski.har@gmail.com>
2018-03-25 18:04:43 -0400

    Revert "file shuffling - created driver directory and removed drivers from main folder"
    This reverts commit 7f666c819fa49890d9ace1b579356963e93917d7.

--- nubaja/hud/build/cxx/cxx_exception_stubs.d
+++
@@ -1,6 +0,0 @@
-cxx_exception_stubs.o: \
- /home/sparky/esp/esp-idf/components/cxx/./cxx_exception_stubs.cpp \
- /home/sparky/esp/esp-idf/nubaja/hud/build/include/sdkconfig.h
-
-/home/sparky/esp/esp-idf/nubaja/hud/build/include/sdkconfig.h:
-/home/sparky/esp/esp-idf/components/cxx/./cxx_exception_stubs.cpp:
